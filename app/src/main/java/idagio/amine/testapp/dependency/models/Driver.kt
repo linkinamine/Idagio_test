51cf7ce81a38f0cbff7f13bc9f2d992bcafc2bfc
Mohamed El Amine Benallouch <linkin.amine88@gmail.com>
2018-04-26 17:10:23 +0200

    creating models creating dependecy Activity

---
+++ app/src/main/java/idagio/amine/testapp/dependency/models/Driver.kt
@@ -0,0 +1,12 @@
+package idagio.amine.testapp.dependency.models
+
+class Driver {
+
+    fun enterCar(): String {
+        return "driver in the car"
+    }
+
+    fun leaveCar(): String {
+        return "driver left the car"
+    }
+}
---
+++ app/src/main/java/idagio/amine/testapp/dependency/models/Engine.kt
@@ -0,0 +1,12 @@
+package idagio.amine.testapp.dependency.models
+
+class Engine {
+
+    fun start(): String {
+        return "Engine running"
+    }
+
+    fun stop(): String {
+        return "Engine stopped"
+    }
+}
