117e1ff047a76b31f205dd0f0611e029660b8afe
Mohamed El Amine Benallouch <linkin.amine88@gmail.com>
2018-04-27 00:05:53 +0200

    refactoring

--- app/src/main/java/idagio/amine/testapp/IdagioApplication.kt
+++ app/src/main/java/idagio/amine/testapp/IdagioApplication.kt
@@ -2,20 +2,19 @@ package idagio.amine.testapp
 
 import android.app.Application
 import android.content.Context
-import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
 
 
-class IdagioTestApplication : Application() {
+open class IdagioApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
+        /*if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
             return
         }
-        LeakCanary.install(this)
+        LeakCanary.install(this)*/
     }
 
 
@@ -23,7 +22,7 @@ class IdagioTestApplication : Application() {
 
     companion object {
         fun getRefWatcher(context: Context): RefWatcher? {
-            val application = context.applicationContext as IdagioTestApplication
+            val application = context.applicationContext as IdagioApplication
             if (application.refWatcher != null)
                 return application.refWatcher
             return null
