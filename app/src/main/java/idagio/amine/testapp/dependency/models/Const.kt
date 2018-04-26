6662d54d92120cc4e5cdd6989614432b6d3dbe65
Mohamed El Amine Benallouch <linkin.amine88@gmail.com>
2018-04-27 00:07:35 +0200

    testing the methods in the di

---
+++ app/src/main/java/idagio/amine/testapp/dependency/models/Const.kt
@@ -0,0 +1,10 @@
+package idagio.amine.testapp.dependency.models
+
+class Const {
+    companion object {
+        const val DRIVER_IN = "driver in the car"
+        const val DRIVER_OUT = "driver left the car"
+        const val CAR_RUNNING = "Engine running"
+        const val CAR_STOPPED = "Engine stopped"
+    }
+}
---
+++ app/src/test/java/idagio/amine/testapp/dependency/DependencyInjectionActivityTest.java
@@ -0,0 +1,51 @@
+package idagio.amine.testapp.dependency;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import idagio.amine.testapp.R;
+import idagio.amine.testapp.dependency.models.Car;
+import idagio.amine.testapp.dependency.models.Const;
+import idagio.amine.testapp.dependency.models.Driver;
+import idagio.amine.testapp.dependency.models.Engine;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class DependencyInjectionActivityTest {
+
+    private DependencyInjectionActivity dependencyInjectionActivity;
+    private Car car;
+
+    @Before
+    public void setUp() throws Exception {
+        dependencyInjectionActivity = Robolectric.setupActivity(DependencyInjectionActivity.class);
+        car = new Car(new Engine(), new Driver());
+    }
+
+    @Test
+    public void stopCarIsSuccessful() throws Exception {
+        TextView carStopped = dependencyInjectionActivity.findViewById(R.id.ad_car_state_tv);
+        carStopped.setText(car.stop());
+        assertEquals(Const.CAR_STOPPED + " " + Const.DRIVER_OUT, carStopped.getText());
+    }
+
+    @Test
+    public void startCarIsSuccessful() throws Exception {
+        car.start();
+        TextView carRunning = dependencyInjectionActivity.findViewById(R.id.ad_car_state_tv);
+        assertEquals(Const.DRIVER_IN + " " + Const.CAR_RUNNING, carRunning.getText());
+    }
+
+    @Test
+    public void startCarIsFailed() throws Exception {
+        car.start();
+        TextView carRunning = dependencyInjectionActivity.findViewById(R.id.ad_car_state_tv);
+        assertEquals(Const.DRIVER_OUT + " " + Const.CAR_RUNNING, carRunning.getText());
+    }
+}
